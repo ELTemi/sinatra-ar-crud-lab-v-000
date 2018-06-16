4a8cc0900438f1205a21648e358f1e671629b67d
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-16 14:06:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,11 +28,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
     erb :edit
   end
 
   patch '/posts/:id' do
-    @post = Post.find(params[:id])
+    
     @post.update
   end
 
