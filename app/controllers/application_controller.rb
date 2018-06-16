9ab8e37a64d5dd62255f6e788830743cc3142f21
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-16 13:31:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
-    Post.create()
+    Post.create(name: params[:name], content: params[:content])
     erb :index
   end
 
